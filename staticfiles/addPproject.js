5a9187c1d0dc1f17bccca12fb11942b5f4c36360
anand-ctrl <anandabhinav.beele17@pec.edu.in>
2021-05-31 22:25:20 +0530

    bug fixed 1

---
+++ staticfiles/addPproject.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
